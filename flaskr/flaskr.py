3fa370a4964a9c06153b7f2109d71e3de30a13f5
icoz <icoz.vt@gmail.com>
2013-05-22 17:23:14 +0400

    tutorial made

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python2
+
 # all the imports
-from flask import Flask, request, session, g, redirect, url_for, \
-	abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 import psycopg2
 from contextlib import closing
 
@@ -18,7 +19,8 @@ app.config.from_object(__name__)
 
 def connect_db():
 	try:
-		conn = psycopg2.connect("dbname='"+DATABASE+"' user='"+USERNAME+"' host='localhost' password='"+PASSWORD+"'")
+		print "dbname='%s' user='%s' host='%s' password='%s'" % (DATABASE, USERNAME, 'localhost', PASSWORD)
+		conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (DATABASE, USERNAME, 'localhost', PASSWORD))
 		return conn
 	except:
 		print "I am unable to connect to the database"
@@ -28,7 +30,56 @@ def init_db():
 		with app.open_resource('schema.sql') as f:
 			db.cursor().execute(f.read())
 			db.commit()
+			print db.cursor().execute("select * from entries")
 
+@app.before_request
+def before_request():
+	g.db = connect_db()
+	
+@app.teardown_request
+def teardown_request(exception):
+	g.db.close()
+
+@app.route('/')
+def show_entries():
+	cur = g.db.cursor()
+	cur.execute('select title, str from entries order by id desc')
+	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+	return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	g.db.cursor().execute('insert into entries (title, str) values (%s, %s)',
+			[request.form['title'], request.form['text']])
+	g.db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+
+@app.template_filter('reverse')
+def reverse_filter(s):
+	return s[::-1]
 
 if __name__ == '__main__':
 	app.run()
--- flaskr/psycopg2_sample.py
+++ flaskr/psycopg2_sample.py
@@ -9,7 +9,7 @@ conn = psycopg2.connect("dbname=test user=test_user")
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
-cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+cur.execute("CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);")
 
 # Pass data to fill a query placeholders and let Psycopg perform
 # the correct conversion (no more SQL injections!)
@@ -17,7 +17,7 @@ cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
 
 # Query the database and obtain data as Python objects
 cur.execute("SELECT * FROM test;")
-cur.fetchone()
+print cur.fetchone()
 # (1, 100, "abc'def")
 
 # Make the changes to the database persistent
