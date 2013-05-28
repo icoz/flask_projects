c741c7a22532c7b94889e4f20b0e5606b3e5c45d
icoz <icoz.vt@gmail.com>
2013-05-28 18:01:40 +0400

    trying to write test-template

--- tests/hello.py
+++ tests/hello.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 
 
-from flask import Flask, url_for
+from flask import Flask, url_for, g, session, request, flash
 from flask import render_template
 
 
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello World!"
+	# session.logged_in = True
+	return render_template("base.html")
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
@@ -30,6 +31,30 @@ def test_urls():
 	p += url_for('path', path='any/where') + "<br>"
 	p += url_for('int', num='123') + "<br>"
 	return p
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'user':
+			# error = 'Invalid username'
+			flash('Invalid username')
+		elif request.form['password'] != 'pass':
+			# error = 'Invalid password'
+			flash('Invalid username')
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('index'))
+	return render_template('base.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
