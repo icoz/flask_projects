f2cb7e4cfb0f412d9e5e16951b38d42611c5aa50
icoz <icoz.vt@gmail.com>
2013-05-29 18:22:23 +0400

    some mods in hello.py, base.html

--- tests/hello.py
+++ tests/hello.py
@@ -1,60 +1,75 @@
 #!/usr/bin/python2
 
 
-from flask import Flask, url_for, g, session, request, flash
+from flask import Flask, url_for, g, session, request, flash, redirect
 from flask import render_template
 
 
+# configuration
+DATABASE = "test"
+DEBUG = True
+SECRET_KEY = "dev key"
+USERNAME = "user"
+PASSWORD = "pass"
+
+# create our little application :)
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route("/")
 def index():
-	# session.logged_in = True
-	return render_template("base.html")
+    # session.logged_in = True
+    return render_template("base.html")
+
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello(name=None):
-	return render_template("hello.html", name=name)
+    return render_template("hello.html", name=name)
+
 
 @app.route("/path/<path:path>")
 def path(path):
-	return "Path = %s" % path
+    return "Path = %s" % path
+
 
 @app.route("/int/<int:num>")
 def int(num):
-	return "Int = %d" % num
+    return "Int = %d" % num
+
 
 @app.route("/test")
 def test_urls():
-	p = url_for('index') + "<br>"
-	p += url_for('path', path='any/where') + "<br>"
-	p += url_for('int', num='123') + "<br>"
-	return p
+    p = url_for('index') + "<br>"
+    p += url_for('path', path='any/where') + "<br>"
+    p += url_for('int', num='123') + "<br>"
+    return p
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'user':
-			# error = 'Invalid username'
-			flash('Invalid username')
-		elif request.form['password'] != 'pass':
-			# error = 'Invalid password'
-			flash('Invalid username')
-		else:
-			session['logged_in'] = True
-			flash('You were logged in')
-			return redirect(url_for('index'))
-	return render_template('base.html')
+    error = None
+    # print request.method, request.form['username'], request.form['password']
+    if request.method == 'POST':
+        if request.form['username'] != USERNAME:
+            # error = 'Invalid username'
+            flash('Invalid username')
+        elif request.form['password'] != PASSWORD:
+            # error = 'Invalid password'
+            flash('Invalid password')
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+    return redirect(url_for('index'))
+    # return render_template('base.html')
+
 
 @app.route('/logout')
 def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('index'))
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
